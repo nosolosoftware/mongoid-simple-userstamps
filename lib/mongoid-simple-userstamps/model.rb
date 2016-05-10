0eed9e151eafa223907bc66c8bce24237144c0e3
Rafael Jurado <rjurado@nosolosoftware.es>
2016-05-10 09:07:19 +0200

    v0.1.2: created_by and updated_by conditionals.

--- lib/mongoid-simple-userstamps/model.rb
+++ lib/mongoid-simple-userstamps/model.rb
@@ -9,11 +9,15 @@ module Mongoid
         base.send(:belongs_to, :updated_by, {polymorphic: true})
 
         base.send(:before_create) do
-          self.created_by = Mongoid::Userstamps::Config.current
+          if Mongoid::Userstamps::Config.current && !self.created_by
+            self.created_by = Mongoid::Userstamps::Config.current
+          end
         end
 
         base.send(:before_update) do
-          self.updated_by = Mongoid::Userstamps::Config.current
+          if Mongoid::Userstamps::Config.current
+            self.updated_by = Mongoid::Userstamps::Config.current
+          end
         end
       end
     end
--- spec/userstamps_spec.rb
+++ spec/userstamps_spec.rb
@@ -13,32 +13,59 @@ describe 'Mongoid::Userstamp' do
     field :title
   end
 
-  before :all do
-    @user = User.create
-    Mongoid::Userstamps::Config.set_current(@user)
-  end
+  context 'when current is defined' do
+    before :all do
+      @user = User.create
+      Mongoid::Userstamps::Config.set_current(@user)
+    end
 
-  context 'when include Mongoid::Userstamps::Model' do
-    it 'add user stamps relations' do
-      post = Post.new
-      expect(post.created_by).to eq nil
-      expect(post.updated_by).to eq nil
+    context 'when include Mongoid::Userstamps::Model' do
+      it 'add user stamps relations' do
+        post = Post.new
+        expect(post.created_by).to eq nil
+        expect(post.updated_by).to eq nil
+      end
+    end
+
+    context 'when create Post' do
+      it 'should save user stamps relations' do
+        post = Post.create
+        expect(post.created_by).to eq(@user)
+      end
+
+      it 'should not overwrite created_by' do
+        user = User.create
+        post = Post.create(created_by: user)
+        expect(post.created_by).to eq(user)
+      end
+    end
+
+    context 'when update Post' do
+      it 'should save user stamps relations' do
+        post = Post.create
+        post.title = "title"
+        post.save
+        expect(post.updated_by).to eq(@user)
+      end
     end
   end
 
-  context 'when create Post' do
-    it 'should save user stamps relations' do
-      post = Post.create
-      expect(post.created_by).to eq(@user)
+  context 'when current is not defined' do
+    before :all do
+      Mongoid::Userstamps::Config.set_current(nil)
     end
-  end
 
-  context 'when update Post' do
-    it 'should save user stamps relations' do
-      post = Post.create
-      post.title = "title"
-      post.save
-      expect(post.updated_by).to eq(@user)
+    it 'should not set created_by' do
+      user = User.create
+      post = Post.create!(created_by: user)
+      expect(post.created_by).to eq(user)
+    end
+
+    it 'should not set updated_by' do
+      user = User.create
+      post = Post.create!
+      post.update_attributes(title: 'title', updated_by: user)
+      expect(post.updated_by).to eq(user)
     end
   end
 end
